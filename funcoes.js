c4e977af61b9ba804821a55d9b726c0636b00d1d
Fellipe Saraiva <fellipe.saraiva@netscanner.com.br>
2021-05-27 21:42:05 -0300

    att funcoes

--- funcoes.js
+++ funcoes.js
@@ -2,12 +2,26 @@ function gera_bloco(numero_posicoes, conjuntos){
 
     var html = "";
     auxLinhas = 0;
-    for (var i = 0; i <= conjuntos; i++){
-        for(j = 0; i <= numero_posicoes; j++){
-            html += "<br>"+auxLinhas;
+    numero_posicoes = parseFloat(numero_posicoes);
+    conjuntos = parseFloat(conjuntos);
+
+    tamanhoconjuntos = conjuntos/2;
+
+
+    for (var i = 0; i < conjuntos; i++){
+        for(j = 0; j < numero_posicoes; j++){
+            html += "<tr>";
+            html += "<td>Linha "+auxLinhas+"</td>";
+            if(j != 0){
+                html += "<td></td>";
+            }else{
+                html += "<td><span class='conj'>Conjunto "+i+"</span></td>";
+            }
+            
+            html += "</tr>";
             auxLinhas++;
         }
     }
 
-    return html;
+    return $("#conteudo").html(html);
 }
