9d5f9c46d68288d8b8c6fe5d65d23d1a71b75269
Fellipe Saraiva <fellipe.saraiva@netscanner.com.br>
2021-05-28 20:24:35 -0300

    att index e functions

--- funcoes.js
+++ funcoes.js
@@ -14,17 +14,68 @@ function gera_bloco(numero_posicoes, conjuntos){
             html += "<tr>";
             html += "<td>Linha "+auxLinhas+"</td>";
             if(j != 0){
-                html += "<td></td>";
+                html += "<td><span id='bloco"+auxLinhas+"'></span></td>";
             }else{
-                html += "<td><span class='conj'>Conjunto "+i+"</span></td>";
+                html += "<td><span id='bloco"+auxLinhas+"'></span><span class='conj'>Conjunto "+i+"</span></td>";
             }
             
             html += "</tr>";
-            auxLinhas++;
             arrayAux.push(auxLinhas);
+            arrayAuxiliarValores.push(-1);
+            auxLinhas++;
         }
         arrayBlocos.push(arrayAux);
     }
 
     return $("#conteudo").html(html);
+}
+
+function alimenta_bloco(sequencia){
+    if(sequencia != ""){
+        var metodo = $("input[name='fcc']:checked").val();
+        var conjuntos = $('#n').val();
+        var numero_posicoes = $('#linha').val();
+        sequencia = parseFloat(sequencia);
+        conjuntos = parseFloat(conjuntos);
+        numero_posicoes = parseFloat(numero_posicoes);
+
+        if(metodo == "fifo"){
+            resultado = sequencia % conjuntos;
+            var stringLog = sequencia+"%"+conjuntos+" = "+resultado;
+
+            if($('#logs').html() != "")
+                $('#logs').html($('#logs').html()+"\n"+stringLog);
+            else
+                $('#logs').html(stringLog);
+
+
+        var aux = arrayBlocos[resultado].length;
+        var i = 0;
+        while(aux != arrayBlocos[resultado].length + numero_posicoes){
+            if(arrayAuxiliarValores[arrayBlocos[resultado][i]] == -1){
+                arrayAuxiliarValores[arrayBlocos[resultado][i]] = sequencia;
+                // alert(arrayAuxiliarValores[arrayBlocos[resultado][i]]);
+                // $("#bloco"+arrayAuxiliarValores[arrayBlocos[resultado][i]]).text(sequencia);
+                for(var j = 0; j < arrayAuxiliarValores.length; j++){
+                    if(arrayAuxiliarValores[j] != -1)
+                        $("#bloco"+j).text(arrayAuxiliarValores[j]);
+                }
+                break;
+            }
+            i++;
+            aux++;
+        }
+        
+        arrayBlocos[resultado]
+
+        
+
+        }else if(metodo == "lru"){
+            
+        }else if(metodo == "lfu"){
+
+        }
+    }else{
+        alert("Preencha a sequência!");
+    }
 }
